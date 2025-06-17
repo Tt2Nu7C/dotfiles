ef1a787cb0f39fdc36cd18bbebf9086054f96b1e
Tt2Nu7C <46764501+Tt2Nu7C@users.noreply.github.com>
2025-06-17 23:51:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,46 @@
-echo "Configuring ZSH\n"
+#!/bin/bash
+
+echo -e "Installing tools\n"
+# Detect distro and install packages
+if [ -f /etc/debian_version ]; then
+    sudo apt update
+    sudo apt install -y zsh zoxide duf wireguard-tools vim batcat tmux git
+elif [ -f /etc/arch-release ]; then
+    sudo pacman -Syu --noconfirm zsh zoxide duf wireguard-tools vim bat tmux git
+else
+    echo "Unsupported distribution. Only Debian and Arch-based distros are supported."
+    exit 1
+fi
+
+echo -e "Configuring ZSH\n"
 mkdir -p ~/.cache
 touch ~/.cache/.histfile
-sudo apt install zsh zoxide duf wireguard vim bat tmux
-sudo mkdir -p /usr/local/share/zsh_conf
-sudo chown $USER:$USER -R /usr/local/share/zsh_conf
 
-git clone https://github.com/ohmyzsh/ohmyzsh
-git clone https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting
+sudo install -d -o "$USER" -g "$USER" /usr/local/share/zsh_conf
 
-mv ohmyzsh /usr/local/share/zsh_conf
-mv zsh-autosuggestions /usr/local/share/zsh_conf
-mv zsh-syntax-highlighting /usr/local/share/zsh_conf
+# Clone ZSH plugins/themes
+repos=(
+    "https://github.com/ohmyzsh/ohmyzsh"
+    "https://github.com/zsh-users/zsh-autosuggestions"
+    "https://github.com/zsh-users/zsh-syntax-highlighting"
+    "https://github.com/romkatv/powerlevel10k.git --depth=1"
+)
 
-echo "Configuring TMUX\n"
+for repo in "${repos[@]}"; do
+    url=${repo%% *}
+    opts=${repo#"$url"}
+    name=$(basename "${url%%.git}")
+    git clone $opts "$url" "/usr/local/share/zsh_conf/$name"
+done
+
+echo -e "Configuring TMUX\n"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp .tmux.conf .vimrc .zshrc ~/
+
+# Copy dotfiles (ensure they exist first)
+cp -v .tmux.conf .vimrc .zshrc ~/ 2>/dev/null || echo "Dotfiles not found, skipped copy."
+
+# Change default shell to ZSH
 chsh -s /bin/zsh
 
-echo "Setup done. Logout and login again to use ZSH\n"
-echo "To install tmux plugins: press prefix + I (capital i, as in Install) to fetch the plugin.\n"
+echo -e "\nSetup done. Logout and login again to use ZSH."
+echo -e "To install tmux plugins: press prefix + I (capital i, as in Install).\n"
