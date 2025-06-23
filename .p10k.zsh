02cb6657ac52830019e109b8e841fc9205403b8a
Tt2Nu7C <46764501+Tt2Nu7C@users.noreply.github.com>
2025-06-24 00:17:42 +0100

    Update .p10k.zsh

--- .p10k.zsh
+++ .p10k.zsh
@@ -549,7 +549,7 @@
 
   ###################[ command_execution_time: duration of the last command ]###################
   # Show duration of the last command if takes at least this many seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=2
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=0
   # Show this many fractional digits. Zero means round to seconds.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=2
   # Execution time color.
